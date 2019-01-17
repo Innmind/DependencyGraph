77248e21743b2bc7e4c078d932b886d04db68197
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-17 18:30:29 +0100

    add tool to keep paths to a root package

---
+++ src/Loader/Dependents/Graph.php
@@ -0,0 +1,124 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\DependencyGraph\Loader\Dependents;
+
+use Innmind\DependencyGraph\{
+    Package,
+    Package\Name,
+};
+use Innmind\Immutable\{
+    SetInterface,
+    Set,
+};
+
+final class Graph
+{
+    private $package;
+    private $parents;
+    private $children;
+    private $cleaned = false;
+
+    private function __construct(Package $package)
+    {
+        $this->package = $package;
+        $this->parents = Set::of(self::class);
+        $this->children = Set::of(self::class);
+    }
+
+    /**
+     * @return SetInterface<Package>
+     */
+    public static function of(Package $root, Package ...$dependents): SetInterface
+    {
+        $root = new self($root);
+        $dependents = Set::of(Package::class, ...$dependents)->reduce(
+            Set::of(self::class),
+            static function(SetInterface $dependents, Package $package): SetInterface {
+                return $dependents->add(
+                    new self($package)
+                );
+            }
+        );
+        self::bind($root, $dependents);
+        $dependents->foreach(static function(self $dependent) use ($dependents): void {
+            self::bind($dependent, $dependents);
+        });
+        $root->keepPaths($root->package->name());
+
+        return $root->collectPackages();
+    }
+
+    private static function bind(self $node, SetInterface $dependents): void
+    {
+        $dependents->foreach(static function(self $dependent) use ($node): void {
+            if ($dependent->package->dependsOn($node->package->name())) {
+                $node->add($dependent);
+            }
+        });
+    }
+
+    private function add(self $parent): void
+    {
+        $this->parents = $this->parents->add($parent);
+        $parent->children = $parent->children->add($this);
+    }
+
+    private function keepPaths(Name $root): void
+    {
+        if ($this->cleaned) {
+            return;
+        }
+
+        $this->cleaned = true;
+
+        $this
+            ->parents
+            ->foreach(static function(self $parent) use ($root): void {
+                $parent->keepRelationsToChildren($root);
+            })
+            ->foreach(static function(self $parent) use ($root): void {
+                $parent->keepPaths($root);
+            });
+    }
+
+    private function keepRelationsToChildren(Name $root): void
+    {
+        $children = $this
+            ->children
+            ->filter(static function(self $child) use ($root): bool {
+                return $child->dependsOn($root);
+            })
+            ->reduce(
+                Set::of(Name::class),
+                static function(SetInterface $children, self $child): SetInterface {
+                    return $children->add($child->package->name());
+                }
+            );
+        $this->package = $this->package->keep($root, ...$children);
+    }
+
+    private function dependsOn(Name $root): bool
+    {
+        if ($this->package->dependsOn($root)) {
+            return true;
+        }
+
+        return $this->children->reduce(
+            false,
+            static function(bool $dependsOn, self $child) use ($root): bool {
+                return $dependsOn || $child->dependsOn($root);
+            }
+        );
+    }
+
+    private function collectPackages(): SetInterface
+    {
+        return $this->parents->reduce(
+            Set::of(Package::class, $this->package),
+            static function(SetInterface $packages, self $parent): SetInterface {
+                return $packages->merge($parent->collectPackages());
+            }
+        );
+    }
+}
---
+++ tests/Loader/Dependents/GraphTest.php
@@ -0,0 +1,102 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\DependencyGraph\Loader\Dependents;
+
+use Innmind\DependencyGraph\{
+    Loader\Dependents\Graph,
+    Package,
+    Package\Name,
+    Package\Relation,
+    Render,
+};
+use Innmind\Url\UrlInterface;
+use Innmind\Immutable\SetInterface;
+use PHPUnit\Framework\TestCase;
+
+class GraphTest extends TestCase
+{
+    public function testKeepPathsLeadingToTheRootPackage()
+    {
+        $packages = Graph::of(
+            new Package(
+                Name::of('vendor/root'),
+                $this->createMock(UrlInterface::class),
+                $this->createMock(UrlInterface::class)
+            ),
+            new Package(
+                Name::of('vendor/libA'),
+                $this->createMock(UrlInterface::class),
+                $this->createMock(UrlInterface::class),
+                new Relation(
+                    Name::of('vendor/root')
+                ),
+                new Relation(
+                    Name::of('watev/lib')
+                )
+            ),
+            new Package(
+                Name::of('vendor/libB'),
+                $this->createMock(UrlInterface::class),
+                $this->createMock(UrlInterface::class),
+                new Relation(
+                    Name::of('vendor/root')
+                ),
+                new Relation(
+                    Name::of('watev/other')
+                )
+            ),
+            new Package(
+                Name::of('watev/foo'),
+                $this->createMock(UrlInterface::class),
+                $this->createMock(UrlInterface::class),
+                new Relation(
+                    Name::of('vendor/libA')
+                ),
+                new Relation(
+                    Name::of('vendor/libB')
+                ),
+                new Relation(
+                    Name::of('vendor/libC')
+                )
+            ),
+            new Package(
+                Name::of('vendor/libC'),
+                $this->createMock(UrlInterface::class),
+                $this->createMock(UrlInterface::class)
+            )
+        );
+
+        $this->assertInstanceOf(SetInterface::class, $packages);
+        $this->assertSame(Package::class, (string) $packages->type());
+        $this->assertCount(4, $packages);
+
+        $expected = <<<DOT
+digraph packages {
+    rankdir="LR";
+    subgraph cluster_vendor {
+        label="vendor"
+        URL="https://packagist.org/packages/vendor/"
+    vendor__root [label="root"];
+    vendor__libA [label="libA"];
+    vendor__libB [label="libB"];
+    }
+    subgraph cluster_watev {
+        label="watev"
+        URL="https://packagist.org/packages/watev/"
+    watev__foo [label="foo"];
+    }
+    vendor__libA -> vendor__root;
+    watev__foo -> vendor__libA;
+    watev__foo -> vendor__libB;
+    vendor__libB -> vendor__root;
+    vendor__root [URL=""];
+    vendor__libA [URL=""];
+    watev__foo [URL=""];
+    vendor__libB [URL=""];
+}
+DOT;
+
+        $this->assertSame($expected, (string) (new Render)(...$packages));
+    }
+}
