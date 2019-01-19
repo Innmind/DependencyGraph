db4e4b5ffdeb2d59c4f383198503d429a021da29
Jérémy DECOOL <jdecool@users.noreply.github.com>
2019-01-19 09:40:54 +0100

    Fix autoload.php path when using Composer global
    The path to the `autoload.php` file is wrong when using `composer global require`.

--- tests/Loader/DependentsTest.php
+++ tests/Loader/DependentsTest.php
@@ -34,6 +34,6 @@ class DependentsTest extends TestCase
 
         $this->assertInstanceOf(SetInterface::class, $packages);
         $this->assertSame(PackageModel::class, (string) $packages->type());
-        $this->assertCount(53, $packages);
+        $this->assertCount(54, $packages);
     }
 }
