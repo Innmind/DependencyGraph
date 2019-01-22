ab83ddf63b8ce32d06bb8fa14da60e35405db7d7
Baptiste Langlade <baptouuuu@gmail.com>
2019-01-22 18:52:42 +0100

    error exit when dot process fails

--- tests/Command/OfTest.php
+++ tests/Command/OfTest.php
@@ -15,12 +15,19 @@ use Innmind\CLI\{
     Command\Options,
     Environment,
 };
-use Innmind\Server\Control\Server\Processes;
+use Innmind\Server\Control\Server\{
+    Processes,
+    Process,
+    Process\ExitCode,
+    Process\Output,
+};
 use function Innmind\HttpTransport\bootstrap as http;
 use Innmind\Url\Path;
+use Innmind\Stream\Writable;
 use Innmind\Immutable\{
     Map,
     Stream,
+    Str,
 };
 use PHPUnit\Framework\TestCase;
 
@@ -83,7 +90,16 @@ USAGE;
                 return (string) $command === "dot '-Tsvg' '-o' 'innmind_cli_dependencies.svg'" &&
                     $command->workingDirectory() === __DIR__.'/../../fixtures' &&
                     (string) $command->input() !== '';
-            }));
+            }))
+            ->willReturn($process = $this->createMock(Process::class));
+        $process
+            ->expects($this->once())
+            ->method('wait')
+            ->will($this->returnSelf());
+        $process
+            ->expects($this->once())
+            ->method('exitCode')
+            ->willReturn(new ExitCode(0));
         $env = $this->createMock(Environment::class);
         $env
             ->expects($this->any())
@@ -92,6 +108,68 @@ USAGE;
         $env
             ->expects($this->never())
             ->method('exit');
+
+        $this->assertNull($command(
+            $env,
+            new Arguments(
+                Map::of('string', 'mixed')
+                    ('package', 'innmind/cli')
+            ),
+            new Options
+        ));
+    }
+
+    public function testExitWithProcessOutputWhenItFails()
+    {
+        $command = new Of(
+            new Dependencies(
+                new Package($this->http)
+            ),
+            new Render,
+            $processes = $this->createMock(Processes::class)
+        );
+        $processes
+            ->expects($this->once())
+            ->method('execute')
+            ->with($this->callback(static function($command): bool {
+                return (string) $command === "dot '-Tsvg' '-o' 'innmind_cli_dependencies.svg'" &&
+                    $command->workingDirectory() === __DIR__.'/../../fixtures' &&
+                    (string) $command->input() !== '';
+            }))
+            ->willReturn($process = $this->createMock(Process::class));
+        $process
+            ->expects($this->once())
+            ->method('wait')
+            ->will($this->returnSelf());
+        $process
+            ->expects($this->once())
+            ->method('exitCode')
+            ->willReturn(new ExitCode(1));
+        $process
+            ->expects($this->once())
+            ->method('output')
+            ->willReturn($output = $this->createMock(Output::class));
+        $output
+            ->expects($this->once())
+            ->method('__toString')
+            ->willReturn('foo');
+        $env = $this->createMock(Environment::class);
+        $env
+            ->expects($this->any())
+            ->method('workingDirectory')
+            ->willReturn(new Path(__DIR__.'/../../fixtures'));
+        $env
+            ->expects($this->once())
+            ->method('exit')
+            ->with(1);
+        $env
+            ->expects($this->once())
+            ->method('error')
+            ->willReturn($error = $this->createMock(Writable::class));
+        $error
+            ->expects($this->once())
+            ->method('write')
+            ->with(Str::of('foo'));
 
         $this->assertNull($command(
             $env,
