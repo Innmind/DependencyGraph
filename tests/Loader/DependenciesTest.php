fa76f41763582c785a1c408c59fcf36e443471bb
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-23 15:55:35 +0100

    fix tests

--- tests/Loader/DependenciesTest.php
+++ tests/Loader/DependenciesTest.php
@@ -25,7 +25,7 @@ class DependenciesTest extends TestCase
 
         $this->assertInstanceOf(SetInterface::class, $packages);
         $this->assertSame(PackageModel::class, (string) $packages->type());
-        $this->assertCount(25, $packages);
+        $this->assertCount(39, $packages);
         $expected = <<<DOT
 digraph packages {
     rankdir="LR";
@@ -39,10 +39,17 @@ digraph packages {
     innmind__url [label="url"];
     innmind__time_warp [label="time-warp"];
     innmind__operating_system [label="operating-system"];
+    innmind__server_status [label="server-status"];
+    innmind__server_control [label="server-control"];
+    innmind__filesystem [label="filesystem"];
+    innmind__socket [label="socket"];
+    innmind__ip [label="ip"];
+    innmind__event_bus [label="event-bus"];
+    innmind__http_transport [label="http-transport"];
+    innmind__http [label="http"];
     innmind__stack_trace [label="stack-trace"];
     innmind__graphviz [label="graphviz"];
     innmind__colour [label="colour"];
-    innmind__filesystem [label="filesystem"];
     }
     subgraph cluster_league {
         label="league"
@@ -61,14 +68,33 @@ digraph packages {
         URL="https://packagist.org/packages/psr/"
     psr__http_message [label="http-message"];
     psr__simple_cache [label="simple-cache"];
+    psr__log [label="log"];
     }
     subgraph cluster_symfony {
         label="symfony"
         URL="https://packagist.org/packages/symfony/"
+    symfony__process [label="process"];
     symfony__filesystem [label="filesystem"];
     symfony__polyfill_ctype [label="polyfill-ctype"];
     symfony__finder [label="finder"];
     symfony__dotenv [label="dotenv"];
+    }
+    subgraph cluster_guzzlehttp {
+        label="guzzlehttp"
+        URL="https://packagist.org/packages/guzzlehttp/"
+    guzzlehttp__guzzle [label="guzzle"];
+    guzzlehttp__promises [label="promises"];
+    guzzlehttp__psr7 [label="psr7"];
+    }
+    subgraph cluster_ralouphie {
+        label="ralouphie"
+        URL="https://packagist.org/packages/ralouphie/"
+    ralouphie__getallheaders [label="getallheaders"];
+    }
+    subgraph cluster_ramsey {
+        label="ramsey"
+        URL="https://packagist.org/packages/ramsey/"
+    ramsey__uuid [label="uuid"];
     }
     innmind__cli -> innmind__stream [color="#87dfa2"];
     innmind__cli -> innmind__immutable [color="#87dfa2"];
@@ -84,6 +110,12 @@ digraph packages {
     innmind__url -> league__uri [color="#085cd3"];
     innmind__time_warp -> innmind__time_continuum [color="#e567f2"];
     innmind__operating_system -> innmind__time_continuum [color="#bb6188"];
+    innmind__operating_system -> innmind__server_status [color="#bb6188"];
+    innmind__operating_system -> innmind__server_control [color="#bb6188"];
+    innmind__operating_system -> innmind__filesystem [color="#bb6188"];
+    innmind__operating_system -> innmind__socket [color="#bb6188"];
+    innmind__operating_system -> innmind__http_transport [color="#bb6188"];
+    innmind__operating_system -> innmind__time_warp [color="#bb6188"];
     innmind__stack_trace -> innmind__immutable [color="#d73f0b"];
     innmind__stack_trace -> innmind__url [color="#d73f0b"];
     innmind__stack_trace -> innmind__graphviz [color="#d73f0b"];
@@ -107,12 +139,41 @@ digraph packages {
     league__uri_manipulations -> league__uri_interfaces [color="#a0cfe9"];
     league__uri_schemes -> psr__http_message [color="#8cd717"];
     league__uri_schemes -> league__uri_parser [color="#8cd717"];
+    innmind__server_status -> innmind__time_continuum [color="#523e48"];
+    innmind__server_status -> symfony__process [color="#523e48"];
+    innmind__server_status -> innmind__immutable [color="#523e48"];
+    innmind__server_status -> innmind__url [color="#523e48"];
+    innmind__server_control -> innmind__url [color="#43d797"];
+    innmind__server_control -> innmind__stream [color="#43d797"];
+    innmind__server_control -> symfony__process [color="#43d797"];
+    innmind__server_control -> psr__log [color="#43d797"];
+    innmind__server_control -> innmind__immutable [color="#43d797"];
+    innmind__socket -> innmind__stream [color="#6b5f49"];
+    innmind__socket -> innmind__ip [color="#6b5f49"];
+    innmind__socket -> innmind__url [color="#6b5f49"];
+    innmind__socket -> innmind__immutable [color="#6b5f49"];
+    innmind__socket -> innmind__event_bus [color="#6b5f49"];
+    innmind__http_transport -> guzzlehttp__guzzle [color="#d9c72a"];
+    innmind__http_transport -> psr__log [color="#d9c72a"];
+    innmind__http_transport -> ramsey__uuid [color="#d9c72a"];
+    innmind__http_transport -> innmind__http [color="#d9c72a"];
+    symfony__filesystem -> symfony__polyfill_ctype [color="#1a4d29"];
+    innmind__event_bus -> innmind__immutable [color="#2d0232"];
+    guzzlehttp__guzzle -> guzzlehttp__promises [color="#e5a30c"];
+    guzzlehttp__guzzle -> guzzlehttp__psr7 [color="#e5a30c"];
+    ramsey__uuid -> symfony__polyfill_ctype [color="#44619b"];
+    innmind__http -> innmind__url [color="#324178"];
+    innmind__http -> innmind__immutable [color="#324178"];
+    innmind__http -> innmind__filesystem [color="#324178"];
+    innmind__http -> innmind__time_continuum [color="#324178"];
+    innmind__http -> innmind__stream [color="#324178"];
+    guzzlehttp__psr7 -> psr__http_message [color="#adcacd"];
+    guzzlehttp__psr7 -> ralouphie__getallheaders [color="#adcacd"];
     innmind__graphviz -> innmind__immutable [color="#39df6f"];
     innmind__graphviz -> innmind__url [color="#39df6f"];
     innmind__graphviz -> innmind__colour [color="#39df6f"];
     innmind__graphviz -> innmind__stream [color="#39df6f"];
     innmind__colour -> innmind__immutable [color="#356a4c"];
-    symfony__filesystem -> symfony__polyfill_ctype [color="#1a4d29"];
     innmind__cli [shape="ellipse", width="0.75", height="0.5", color="#87dfa2", URL="https://packagist.org/packages/innmind/cli"];
     innmind__stream [shape="ellipse", width="0.75", height="0.5", color="#5eb3ec", URL="https://packagist.org/packages/innmind/stream"];
     innmind__immutable [shape="ellipse", width="0.75", height="0.5", color="#a7e599", URL="https://packagist.org/packages/innmind/immutable"];
@@ -133,11 +194,25 @@ digraph packages {
     league__uri_schemes [shape="ellipse", width="0.75", height="0.5", color="#8cd717", URL="https://packagist.org/packages/league/uri-schemes"];
     league__uri_query_parser [shape="ellipse", width="0.75", height="0.5", color="#1e8c91", URL="https://packagist.org/packages/league/uri-query-parser"];
     psr__simple_cache [shape="ellipse", width="0.75", height="0.5", color="#01186e", URL="https://packagist.org/packages/psr/simple-cache"];
-    innmind__graphviz [shape="ellipse", width="0.75", height="0.5", color="#39df6f", URL="https://packagist.org/packages/innmind/graphviz"];
-    innmind__colour [shape="ellipse", width="0.75", height="0.5", color="#356a4c", URL="https://packagist.org/packages/innmind/colour"];
+    innmind__server_status [shape="ellipse", width="0.75", height="0.5", color="#523e48", URL="https://packagist.org/packages/innmind/server-status"];
+    innmind__server_control [shape="ellipse", width="0.75", height="0.5", color="#43d797", URL="https://packagist.org/packages/innmind/server-control"];
+    innmind__socket [shape="ellipse", width="0.75", height="0.5", color="#6b5f49", URL="https://packagist.org/packages/innmind/socket"];
+    innmind__http_transport [shape="ellipse", width="0.75", height="0.5", color="#d9c72a", URL="https://packagist.org/packages/innmind/http-transport"];
+    symfony__process [shape="ellipse", width="0.75", height="0.5", color="#9cadc5", URL="https://packagist.org/packages/symfony/process"];
+    psr__log [shape="ellipse", width="0.75", height="0.5", color="#e9a8b6", URL="https://packagist.org/packages/psr/log"];
     symfony__filesystem [shape="ellipse", width="0.75", height="0.5", color="#1a4d29", URL="https://packagist.org/packages/symfony/filesystem"];
     symfony__finder [shape="ellipse", width="0.75", height="0.5", color="#952a8a", URL="https://packagist.org/packages/symfony/finder"];
     symfony__polyfill_ctype [shape="ellipse", width="0.75", height="0.5", color="#96e3a7", URL="https://packagist.org/packages/symfony/polyfill-ctype"];
+    innmind__ip [shape="ellipse", width="0.75", height="0.5", color="#fc3785", URL="https://packagist.org/packages/innmind/ip"];
+    innmind__event_bus [shape="ellipse", width="0.75", height="0.5", color="#2d0232", URL="https://packagist.org/packages/innmind/event-bus"];
+    guzzlehttp__guzzle [shape="ellipse", width="0.75", height="0.5", color="#e5a30c", URL="https://packagist.org/packages/guzzlehttp/guzzle"];
+    ramsey__uuid [shape="ellipse", width="0.75", height="0.5", color="#44619b", URL="https://packagist.org/packages/ramsey/uuid"];
+    innmind__http [shape="ellipse", width="0.75", height="0.5", color="#324178", URL="https://packagist.org/packages/innmind/http"];
+    guzzlehttp__promises [shape="ellipse", width="0.75", height="0.5", color="#e89948", URL="https://packagist.org/packages/guzzlehttp/promises"];
+    guzzlehttp__psr7 [shape="ellipse", width="0.75", height="0.5", color="#adcacd", URL="https://packagist.org/packages/guzzlehttp/psr7"];
+    ralouphie__getallheaders [shape="ellipse", width="0.75", height="0.5", color="#373d23", URL="https://packagist.org/packages/ralouphie/getallheaders"];
+    innmind__graphviz [shape="ellipse", width="0.75", height="0.5", color="#39df6f", URL="https://packagist.org/packages/innmind/graphviz"];
+    innmind__colour [shape="ellipse", width="0.75", height="0.5", color="#356a4c", URL="https://packagist.org/packages/innmind/colour"];
 }
 DOT;
 
