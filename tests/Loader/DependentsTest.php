ff27127adf3b5bf8a5bb94101374bbc6df8df147
Baptiste Langlade <baptouuuu@gmail.com>
2019-02-24 11:03:29 +0100

    abandon of innmind/config

--- tests/Loader/DependentsTest.php
+++ tests/Loader/DependentsTest.php
@@ -34,6 +34,6 @@ class DependentsTest extends TestCase
 
         $this->assertInstanceOf(SetInterface::class, $packages);
         $this->assertSame(PackageModel::class, (string) $packages->type());
-        $this->assertCount(58, $packages);
+        $this->assertCount(57, $packages);
     }
 }
